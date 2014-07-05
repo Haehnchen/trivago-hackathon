ac471494ee0ce3e3877af7df6787bfe3ec8f51ec
Daniel Espendiller <daniel@espendiller.net>
2014-07-05 18:03:09 +0200

    add javascript for tooltip

---
+++ web-ui/web/scripts/tooltip_helper.js
@@ -0,0 +1,37 @@
+
+var hotelApiBackend = 'hotel.json';
+
+var addMarkerTooltipEvents = function(marker, markerTemplate) {
+
+    var tooltip = new Tooltip({map: marker.map}, marker);
+    tooltip.bindTo("text", marker, "tooltip");
+
+    google.maps.event.addListener(marker, 'mouseover', function() {
+
+        //$.getJSON("http://192.168.245.141:8080/Webservice/hotel/dortmund", function(data) {
+        $.getJSON(hotelApiBackend, function(data) {
+            var foo = markerTemplate;
+
+            for (var k in data){
+                foo = foo.replace('%' + k + '%', data[k]);
+            }
+
+            tooltip.addTip(foo);
+            tooltip.getPos2(marker.getPosition());
+        });
+
+    });
+
+    google.maps.event.addListener(marker, 'mouseout', function() {
+        tooltip.removeTip();
+    });
+
+}
+
+var addMarkerWithTooltip = function(options) {
+
+    var marker = new google.maps.Marker(options);
+    addMarkerTooltipEvents(marker, options.markerTemplate);
+
+    return marker;
+};
