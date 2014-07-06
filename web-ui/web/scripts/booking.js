1558980206a4c1ba3727a7be3bbd74987be6da81
Daniel Espendiller <daniel@espendiller.net>
2014-07-06 10:01:01 +0200

    simplify hotel result

--- web-ui/web/scripts/booking.js
+++ web-ui/web/scripts/booking.js
@@ -1,7 +1,25 @@
 $(function() {
 
-    $( document ).on( "click", ".remove-circle", function() {
+
+    var ids = [];
+
+    var hotel_result = function(data) {
+
+        if ($("#hotel-result").length > 0){
+            $("#hotel-result").remove();
+        }
+
+        $('body').append($(data));
+    }
+
+    $(document).on("click", ".remove-circle", function() {
         $(this).closest('.hotel-result').remove();
+    });
+
+    $("#maps_canvas").on("selectMarker", function(event, param1) {
+        console.log(param1);
+        ids.push({'id': Math.floor((Math.random() * 100) + 1)});
+        $.post("/hotel/result", { hotels: ids }).done(hotel_result);
     });
 
     $( "#hotel-result-button" ).click(function(e) {
@@ -10,15 +28,7 @@ $(function() {
 
         $.ajax({
             url: "/hotel/result"
-        }).done(function(data) {
-
-            if ($("#hotel-result").length > 0){
-                $("#hotel-result").remove();
-            }
-
-            $('body').append($(data));
-
-        });
+        }).done(hotel_result);
 
     });
 
